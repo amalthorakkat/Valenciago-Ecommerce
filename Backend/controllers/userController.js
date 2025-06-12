9886bf0eb61697085e339122acb2a640f3c2d68d
amalthorakkat <amalthorakkat@gmail.com>
2025-06-12 11:08:55 +0530

    Changes in Checkout Page and Related Backend

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,113 +1,8 @@
-// const User = require("../models/userModels");
-
-// exports.addAddress = async (req, res) => {
-
-// console.log("from add to cart")
-//   try {
-
-//     const { street, city, state, zip, country, isSelected } = req.body;
-
-//     const user = await User.findById(req.user.id);
-
-//     if (!user) return res.status(404).json({ message: "User not found" });
-
-//     // If new address isSelected, deselect others
-//     if (isSelected) {
-//       user.address.forEach((addr) => (addr.isSelected = false));
-//     }
-
-//     user.address.push({ street, city, state, zip, country, isSelected });
-//     await user.save();
-
-//     res.status(200).json(user);
-//   } catch (error) {
-//     console.log(error)
-//     res.status(500).json({
-//       message: "Failed to add address",
-//       error: error.message,
-//     });
-//   }
-// };
-
-// exports.updateAddress = async (req, res) => {
-//   try {
-//     const { addressId } = req.params;
-//     const { street, city, state, zip, country, isSelected } = req.body;
-
-//     const user = await User.findById(req.user.id);
-//     if (!user) return res.status(404).json({ message: "User not found" });
-
-//     if (isSelected) {
-//       // Deselect all others
-//       user.address.forEach((addr) => (addr.isSelected = false));
-//     }
-
-//     const address = user.address.id(addressId);
-//     if (!address) return res.status(404).json({ message: "Address not found" });
-
-//     address.street = street || address.street;
-//     address.city = city || address.city;
-//     address.state = state || address.state;
-//     address.zip = zip || address.zip;
-//     address.country = country || address.country;
-//     if (typeof isSelected === "boolean") address.isSelected = isSelected;
-
-//     await user.save();
-//     res.status(200).json(user.address);
-//   } catch (error) {
-//     res.status(500).json({ message: "Failed to update address", error: error.message });
-//   }
-// };
-
-// exports.getSelectedAddress = async (req, res) => {
-//   try {
-//     const user = await User.findById(req.user.id);
-//     const selected = user.address.find((addr) => addr.isSelected);
-//     res.status(200).json(selected || {});
-//   } catch (error) {
-//     res.status(500).json({ message: "Failed to fetch selected address" });
-//   }
-// };
-
-// exports.getAddresses = async (req, res) => {
-//   try {
-//     const user = await User.findById(req.user.id).select("address");
-//     if (!user) {
-//       return res.status(404).json({ message: "User not found" });
-//     }
-//     res.json(user.address || []);
-//   } catch (err) {
-//     res
-//       .status(500)
-//       .json({ message: "Failed to get addresses", error: err.message });
-//   }
-// };
-
-// exports.deleteAddress = async (req, res) => {
-//   try {
-//     const { addressId } = req.params;
-//     const user = await User.findById(req.user.id);
-//     if (!user) return res.status(404).json({ message: "User not found" });
-
-//     user.address.id(addressId).remove();
-
-//     // If deleted address was selected, set first address as selected (if any)
-//     if (!user.address.some(addr => addr.isSelected) && user.address.length > 0) {
-//       user.address[0].isSelected = true;
-//     }
-
-//     await user.save();
-//     res.status(200).json(user.address);
-//   } catch (error) {
-//     res.status(500).json({ message: "Failed to delete address", error: error.message });
-//   }
-// };
-
 const User = require("../models/userModels");
 
 exports.addAddress = async (req, res) => {
   try {
-    const { street, city, state, zip, country } = req.body;
+    const { name, street, city, state, zip, country, phone } = req.body;
     const user = await User.findById(req.user.id);
     if (!user) return res.status(404).json({ message: "User not found" });
 
@@ -115,20 +10,24 @@ exports.addAddress = async (req, res) => {
     // user.address.forEach((addr) => (addr.isSelected = false)); // Deselect all
     if (user.address.length == 0) {
       user.address.push({
+        name,
         street,
         city,
         state,
         zip,
         country,
+        phone,
         isSelected: true,
       });
     } else {
       user.address.push({
+        name,
         street,
         city,
         state,
         zip,
         country,
+        phone,
         isSelected: false,
       });
     }
@@ -146,7 +45,8 @@ exports.addAddress = async (req, res) => {
 exports.updateAddress = async (req, res) => {
   try {
     const { addressId } = req.params;
-    const { street, city, state, zip, country, isSelected } = req.body;
+    const { name, street, city, state, zip, country, phone, isSelected } =
+      req.body;
 
     const user = await User.findById(req.user.id);
     if (!user) return res.status(404).json({ message: "User not found" });
@@ -158,11 +58,13 @@ exports.updateAddress = async (req, res) => {
     const address = user.address.id(addressId);
     if (!address) return res.status(404).json({ message: "Address not found" });
 
+    address.name = name || address.name
     address.street = street || address.street;
     address.city = city || address.city;
     address.state = state || address.state;
     address.zip = zip || address.zip;
     address.country = country || address.country;
+    address.phone = phone || address.phone
 
     if (typeof isSelected === "boolean") address.isSelected = isSelected;
 
--- Backend/models/userModels.js
+++ Backend/models/userModels.js
@@ -1,51 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const bcrypt = require("bcryptjs");
-
-// const userSchema = new mongoose.Schema({
-//   username: {
-//     type: String,
-//     required: true,
-//   },
-//   password: String,
-//   email: String,
-//   role: {
-//     type: String,
-//     default: "User",
-//   },
-//   address: [
-//     {
-//       street: String,
-//       city: String,
-//       state: String,
-//       zip: String,
-//       country: String,
-//       isSelected:{
-//           type:Boolean,
-//           default:true,
-//       },
-//     },
-//   ],
-
-// //   address: {
-// //     street: String,
-// //     city: String,
-// //     state: String,
-// //     zip: String,
-// //     country: String,
-// //   },
-// });
-
-// userSchema.pre("save", async function (next) {
-//   if (!this.isModified("password")) {
-//     return next;
-//   }
-//   this.password = await bcrypt.hash(this.password, 10);
-// });
-
-// const userModel = mongoose.model("User", userSchema);
-// module.exports = userModel;
-
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
@@ -56,11 +8,13 @@ const userSchema = new mongoose.Schema({
   role: { type: String, default: "User" },
   address: [
     {
+      name:String,
       street: String,
       city: String,
       state: String,
       zip: String,
       country: String,
+      phone:String,
       isSelected: { type: Boolean, default: false },
     },
   ],
@@ -73,4 +27,3 @@ userSchema.pre("save", async function (next) {
 });
 
 module.exports = mongoose.model("User", userSchema);
-
